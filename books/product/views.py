d52e6a79393c73c138e65617a1a5ed953766a228
unknown <himanshubakshi99@gmail.com>
2019-10-01 00:01:13 +0530

    Search Implemented

--- books/product/views.py
+++ books/product/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from .models import Product, ProductImages, Category
 from django.core.paginator import Paginator
 from django.db.models import Count
+from django.db.models import Q  # For searching purpose
+from django.shortcuts import get_object_or_404  # we will replace get by get_object_or_404
 
 
 # Create your views here.
@@ -16,8 +18,18 @@ def productlist(request, category_slug=None):
     template = 'Product/product_list.html'
 
     if category_slug:
-        category = Category.objects.get(slug=category_slug)
+        category = get_object_or_404(Category, slug=category_slug)
         product_list = product_list.filter(category=category)
+
+    search_query = request.GET.get('q')
+    if search_query:
+        product_list = product_list.filter(
+            Q(name__icontains=search_query) |
+            Q(description__icontains=search_query) |
+            Q(condition__icontains=search_query) |
+            Q(brand__brand_name__icontains=search_query)
+
+        )
 
     paginator = Paginator(product_list, 1)  # show only 10 products on 1 page
     page = request.GET.get('page')
@@ -31,7 +43,7 @@ def productlist(request, category_slug=None):
 def productdetail(request, product_slug):
     # print(product_slug)
     # product_detail = Product.objects.filter(id=id).first()
-    product_detail = Product.objects.get(slug=product_slug)
+    product_detail = get_object_or_404(Product, slug=product_slug)
     productimages = ProductImages.objects.filter(product=product_detail)
     template = 'Product/product_detail.html'
     context = {'product_detail': product_detail, 'product_images': productimages}
